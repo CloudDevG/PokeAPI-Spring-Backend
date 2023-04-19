280edc9895001de42b80ee05d91d0e39b1de30e6
CloudDevG <28709311+CloudDevG@users.noreply.github.com>
2023-04-18 22:37:52 -0400

    Adding missing @JsonProperty annotations to class 'NamedApiResource' to ensure object continuity

--- src/main/java/com/kennected/pokedex/entity/NamedApiResource.java
+++ src/main/java/com/kennected/pokedex/entity/NamedApiResource.java
@@ -1,14 +1,17 @@
 package com.kennected.pokedex.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public class NamedApiResource implements Serializable{
+    @JsonProperty("name")
     private String name;
 
+    @JsonProperty("url")
     private String url;
 
     public NamedApiResource() {
