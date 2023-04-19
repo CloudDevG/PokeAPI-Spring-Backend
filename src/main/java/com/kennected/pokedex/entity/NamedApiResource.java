cbba944accf50cfbc5cbf1a8c66415bdcdd86c5b
CloudDevG <28709311+CloudDevG@users.noreply.github.com>
2023-04-19 13:27:19 -0400

    Removing non-utilized  setter methods from 'NamedApiResource' class

--- src/main/java/com/kennected/pokedex/entity/NamedApiResource.java
+++ src/main/java/com/kennected/pokedex/entity/NamedApiResource.java
@@ -26,18 +26,9 @@ public class NamedApiResource implements Serializable{
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getUrl() {
         return url;
     }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
 
     @Override
     public String toString() {
